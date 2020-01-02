f6ea0c542c1c9e1f03e40049074cd1fa0f697bd8
amy-actually <amybwest@gmail.com>
2020-01-02 12:55:26 -0500

    restructured for dev cycle

--- .dev/.config/scripts/set_timezone.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-apk add --update --no-cache tzdata
-TZ=$1
-ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
--- staging/scripts/create_user.sh
+++ staging/scripts/create_user.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-
 APP_USER=$1
 APP_GROUP=$2
 APP_USER_ID=$3
